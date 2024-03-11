ab8943d015cefbaebe30d7592ec73461869ad633
Raghunath Das <raghunathdas704@gmail.com>
2024-03-11 18:27:08 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="text-5xl text-center mt-3 font-bold text-white">Calculator</h1>
+      <div className="h-40 w-40 bg-white">Centered Div</div>
+    </>
+  )
+}
+
+export default App
